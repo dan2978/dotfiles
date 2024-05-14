e131656e76e6eff2d9712587f736a64f369fc020
dan2978 <dan-2978@ya.ru>
2024-05-14 21:48:15 +0400

    Comments on the list of LSP servers have been updated. Treesitter configuration has been formatted

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,14 +3,14 @@ local mason_lspconfig = require'mason-lspconfig'
 local lspconfig = require'lspconfig'
 
 local servers = {
-    'clangd',           -- C/C++
+    'clangd',           -- C, C++
     'rust_analyzer',    -- Rust
     'cmake',            -- CMake
-    'autotools_ls',     -- Make, Automake
+    'autotools_ls',     -- Make, Automake, Autoconf
     'pyright',          -- Python
     'lua_ls',           -- LUA
-    'verible',          -- Verilog/SystemVerilog
-    'texlab',           -- TeX/LaTeX/BibTeX
+    'verible',          -- Verilog, SystemVerilog
+    'texlab',           -- TeX, LaTeX, BibTeX
 }
 
 mason.setup()
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,13 @@
 local treesitter = require'nvim-treesitter.configs'
 
 treesitter.setup {
-    ensure_installed = {'c', 'cpp', 'rust', 'cmake', 'make', 'python', 'verilog', 'latex', 'bibtex'},
+    ensure_installed = {
+        'c', 'cpp', 'rust', 'cmake', 'make','python', 'lua', 'verilog', 'latex',
+        'bibtex'
+    },
     sync_install = false,
     auto_install = true,
-    ignore_install = {''},
-	highlight = {
-		enable = true
-	},
-    indent = {
-        enable = {'make', 'python'}
-    }
+    ignore_install = { '' },
+	highlight = { enable = true },
+    indent = { enable = { 'make', 'python' } }
 }
